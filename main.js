e00eb3c2bfc25fba8e76f37c831c0b30bc2af307
MetalSnow <aymanetrend@gmail.com>
2024-06-16 15:05:40 +0800

    Add validity check for empty inputs

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-const display = document.querySelector(".display");
-const dialog = document.querySelector("dialog");
+const display = document.querySelector('.display');
+const dialog = document.querySelector('dialog');
+const form = document.querySelector('form');
+const error = document.querySelector('.error');
 
 //btns
-const openBtn = document.querySelector(".modal-btn");
-const closeBtn = document.querySelector(".close-btn");
-const addBtn = document.querySelector("#add-book");
+const openBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const addBtn = document.querySelector('#add-book');
 
 //inputs
-const inputs = document.querySelectorAll("input");
-const authorInput = document.querySelector("#author");
-const titleInput = document.querySelector("#title");
-const pagesInput = document.querySelector("#pages");
+const inputs = document.querySelectorAll('input');
+const authorInput = document.querySelector('#author');
+const titleInput = document.querySelector('#title');
+const pagesInput = document.querySelector('#pages');
 
 document.body.appendChild(display);
 
 //dialog buttons
-openBtn.addEventListener("click", () => {
+openBtn.addEventListener('click', () => {
   dialog.showModal();
 });
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener('click', () => {
   dialog.close();
 });
 
@@ -27,16 +29,16 @@ const myLibrary = [];
 
 class Book {
   constructor(author, title, pages, read) {
-    (this.author = author || "Unkown"),
-      (this.title = title || "NO TITLE"),
-      (this.pages = pages || "0"),
+    (this.author = author),
+      (this.title = title),
+      (this.pages = pages),
       (this.read = read);
   }
   toggleStatus() {
-    if (this.read === "Not Read") {
-      this.read = "Read";
+    if (this.read === 'Not Read') {
+      this.read = 'Read';
     } else {
-      this.read = "Not Read";
+      this.read = 'Not Read';
     }
   }
 }
@@ -46,39 +48,55 @@ function addBookToLibrary() {
     authorInput.value,
     titleInput.value.toUpperCase(),
     pagesInput.value,
-    "Not Read"
+    'Not Read'
   );
   myLibrary.push(newBook);
 }
 
-addBtn.addEventListener("click", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
+
+  // reset validity check
+  inputs.forEach((input) => (input.style.border = ''));
+  error.textContent = '';
+  error.classList.remove('active');
+
+  const isValid =
+    authorInput.validity.valid &&
+    titleInput.validity.valid &&
+    pagesInput.validity.valid;
+
+  if (!isValid) {
+    showError();
+    return;
+  }
+
   addBookToLibrary();
   displayBooks();
 
   //clear inputs
   inputs.forEach((input) => {
-    input.value = "";
+    input.value = '';
   });
 });
 
 function displayBooks() {
-  display.innerHTML = "";
+  display.innerHTML = '';
 
   myLibrary.map((book, index) => {
-    const bookElement = document.createElement("div");
-    const deleteBtn = document.createElement("button");
-    const statusBtn = document.createElement("button");
-    const h2 = document.createElement("h2");
-    const p1 = document.createElement("p");
-    const p2 = document.createElement("p");
-    const p3 = document.createElement("p");
+    const bookElement = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+    const statusBtn = document.createElement('button');
+    const h2 = document.createElement('h2');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
 
-    bookElement.classList.add("book");
+    bookElement.classList.add('book');
     bookElement.dataset.number = index;
 
-    deleteBtn.textContent = "Delete Book";
-    statusBtn.textContent = "Read Status";
+    deleteBtn.textContent = 'Delete Book';
+    statusBtn.textContent = 'Read Status';
 
     h2.innerText = book.title;
     p1.innerText = `Author: ${book.author}`;
@@ -87,23 +105,36 @@ function displayBooks() {
 
     function deleteBook() {
       myLibrary.splice(index, 1);
-      deleteBtn.removeEventListener("click", deleteBook);
+      deleteBtn.removeEventListener('click', deleteBook);
       displayBooks();
     }
 
-    deleteBtn.addEventListener("click", deleteBook);
+    deleteBtn.addEventListener('click', deleteBook);
 
-    statusBtn.addEventListener("click", () => {
+    statusBtn.addEventListener('click', () => {
       book.toggleStatus();
       p3.innerText = `Mark as: ${book.read}`;
-      if (book.read === "Read") {
-        statusBtn.style.opacity = "0.7";
+      if (book.read === 'Read') {
+        statusBtn.style.opacity = '0.7';
       } else {
-        statusBtn.style.opacity = "1";
+        statusBtn.style.opacity = '1';
       }
     });
 
     bookElement.append(h2, p1, p2, p3, statusBtn, deleteBtn);
     display.appendChild(bookElement);
+  });
+}
+
+function showError() {
+  error.textContent = 'All Fields Are Mandatory!';
+  error.classList.add('active');
+
+  inputs.forEach((input) => {
+    if (input.value === '') {
+      input.style.border = '1px solid red';
+    } else {
+      input.style.border = '';
+    }
   });
 }
